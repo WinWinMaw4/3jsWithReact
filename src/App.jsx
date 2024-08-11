ab3523c88d755faf5d190c8fda52f657476c0de6
WinWinMaw4 <mawinwinmaw4@gmail.com>
2024-08-11 19:45:54 +0630

    Basic Sence

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Canvas } from "@react-three/fiber"
+import Experience from "./Experience"
+import { OrbitControls } from "@react-three/drei"
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>Hello world</div>
+    <div id="canvas-container" className="fixed top-0 left-0 w-full h-full">
+      <Canvas 
+
+        camera={{
+            fov:45,
+          near:0.1,
+          far:200,
+          position:[0,-6 ,15]
+        }}
+      >
+        <Experience />
+        <OrbitControls makeDefault  />
+      </Canvas>
+    </div>
   )
 }
 
